3a3248e0996735613a5a56208931d6c3e500faad
Noman Sheikh <nomansheak3@gmail.com>
2021-09-07 16:27:06 +0600

    add 3D file support

--- app/Http/Controllers/Api/NftController.php
+++ app/Http/Controllers/Api/NftController.php
@@ -8,6 +8,7 @@ use App\Models\Nft;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Str;
 use Vinkla\Hashids\Facades\Hashids;
 
 class NftController extends Controller
@@ -39,8 +40,8 @@ class NftController extends Controller
             'supply' => $request->supply,
             'fil_address' => $request->fil_address,
             'network' => $request->network,
-            'asset' => $request->file('asset')->store('/'),
-            'asset_type' => $request->file('asset')->getMimeType()
+            'asset' => $request->file('asset')->storeAs('/', $this->getFileName($request)),
+            'asset_type' => $this->getFileMimeType($request)
         ]);
 
         return response()->json([
@@ -48,7 +49,7 @@ class NftController extends Controller
         ], 200);
     }
 
-    public function show(string $nft)
+    public function show(string $nft): NftResource
     {
         $nft = Nft::query()
             ->where('id', Hashids::decode($nft))
@@ -56,5 +57,29 @@ class NftController extends Controller
             ->firstOrFail();
 
         return new NftResource($nft);
+    }
+
+    private function getFileMimeType(Request $request): string
+    {
+        return $this->is3DFile($request)
+            ? 'model/gltf-binary'
+            : $request->file('asset')->getMimeType();
+    }
+
+    private function getFileName(Request $request): string
+    {
+        return Str::random(40) . '.' . $this->getFileExtension($request);
+    }
+
+    private function is3DFile(Request $request): bool
+    {
+        $file = $this->getFileExtension($request);
+
+        return $file === 'gltf' || $file === 'glb';
+    }
+
+    private function getFileExtension(Request $request): string
+    {
+        return $request->file('asset')->getClientOriginalExtension();
     }
 }
